985ada118f8b56ab84ab9253820b52596fa93b9c
VoltrefferNL <nielsdevisser@hotmail.com>
2020-05-01 15:25:13 +0100

    Completed DisplayPage

--- src/components/DisplayPage.jsx
+++ src/components/DisplayPage.jsx
@@ -3,7 +3,25 @@ import { Router } from "@reach/router";
 
 class DisplayPage extends Component {
   render() {
-    return <div>Hello</div>;
+    return (
+      <div>
+        <div>
+          <ul>
+            <div className="query-area">
+              <li>Votes</li>
+              <li>Date Created</li>
+              <li>Comments</li>
+            </div>
+            <div className="articleslist-area">
+              <li>List of Articles on display here</li>
+            </div>
+            <div className="content-area">
+              The content here (Article, comments)
+            </div>
+          </ul>
+        </div>
+      </div>
+    );
   }
 }
 
