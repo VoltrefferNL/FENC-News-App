e7a15957fbc36bded714e448d13a7672ca390fea
VoltrefferNL <nielsdevisser@hotmail.com>
2020-05-03 21:26:49 +0100

    Fixed css positioning MenuBar

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -15,40 +15,45 @@ class Comments extends Component {
 
   render() {
     const { comments } = this.state;
-    return (  
-    
-    <div className="Comment-template">
-    <h2>Comments</h2>
-    <h3> Comment by $User$</h3>
-    <form method="post" onSubmit={e => this.addNewComment(e)}><label>Comment:</label><textarea name="body" id="body"></textarea>
-          <input type="submit" value="submitNewComment" name="submitNewComment"></input>
-    </form>
+    return (
+      <div className="Comment-template">
+        <h2>Comments</h2>
+        <h3> Comment by $User$</h3>
+        <form method="post" onSubmit={(e) => this.addNewComment(e)}>
+          <label>Comment:</label>
+          <textarea name="body" id="body"></textarea>
+          <input
+            type="submit"
+            value="submitNewComment"
+            name="submitNewComment"
+          ></input>
+        </form>
 
-    {this.state.comments 
-    ? comments.map(({ author, created_at, votes, body, comment_id }) => {
-      return (
-        <div className="comment-card-holder" key={`${comment_id}`}>
-          <div>
-            <ul>
-              <li>Up</li>
-              <li>{votes}</li>
-              <li>Down</li>
-            </ul>
-          </div>
-          <div>
-            <ul>
-              <li>{body}</li>
-              <li>
-                By {author} at {created_at}
-              </li>
-            </ul>
-          </div>
-        </div>
-          );
-    })
-  : "Loading..." } 
-  
-  </div>)
+        {this.state.comments
+          ? comments.map(({ author, created_at, votes, body, comment_id }) => {
+              return (
+                <div className="comment-card-holder" key={`${comment_id}`}>
+                  <div>
+                    <ul>
+                      <li>Up</li>
+                      <li>{votes}</li>
+                      <li>Down</li>
+                    </ul>
+                  </div>
+                  <div>
+                    <ul>
+                      <li>{body}</li>
+                      <li>
+                        By {author} at {created_at}
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              );
+            })
+          : "Loading..."}
+      </div>
+    );
+  }
 }
-
 export default Comments;
--- src/components/MenuBar.jsx
+++ src/components/MenuBar.jsx
@@ -25,31 +25,33 @@ class MenuBar extends Component {
           <div className="logo">
             <Link to="/">NC News</Link>
           </div>
-          <ul className="menu">
-            <li className="menu-dropdown">
-              <select
-                id="topics"
-                onChange={(e) => {
-                  this.handleChange(e.target.value);
-                }}
-              >
-                <option value="">Choose your topic</option>
-                {topics.map(({ slug }) => {
-                  return (
-                    <option key={slug} value={slug}>
-                      {utils.capitalizeFirstLetter(slug)}
-                    </option>
-                  );
-                })}
-              </select>
-            </li>
-            <li className="menu-link">
-              <Link to="/">Home</Link>
-            </li>
-            <li className="menu-link">
-              <Link to="/articles">Articles</Link>
-            </li>
-          </ul>
+          <div className="menu-container">
+            <ul className="menu">
+              <li className="menu-dropdown">
+                <select
+                  id="topics"
+                  onChange={(e) => {
+                    this.handleChange(e.target.value);
+                  }}
+                >
+                  <option value="">Choose your topic</option>
+                  {topics.map(({ slug }) => {
+                    return (
+                      <option key={slug} value={slug}>
+                        {utils.capitalizeFirstLetter(slug)}
+                      </option>
+                    );
+                  })}
+                </select>
+              </li>
+              <li className="menu-link">
+                <Link to="/">Home</Link>
+              </li>
+              <li className="menu-link">
+                <Link to="/articles">Articles</Link>
+              </li>
+            </ul>
+          </div>
           <div className="userInfo">Welcome {user}</div>
         </div>
       </nav>
