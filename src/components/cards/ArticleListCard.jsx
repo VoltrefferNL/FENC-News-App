937b20681b8359fa6b7ba197fa47e2d5b1526cae
VoltrefferNL <nielsdevisser@hotmail.com>
2020-05-11 14:38:12 +0100

    Added lists for both comments and articles, removed some unused-css

--- src/components/cards/ArticleListCard.jsx
+++ src/components/cards/ArticleListCard.jsx
@@ -9,54 +9,58 @@ const ArticleListCard = (props) => {
   if (isLoading) return <LoadingMessage />;
   if (err) return <ErrorMessage err={err} />;
   else
-    return articles.map(
-      ({
-        author,
-        title,
-        article_id,
-        topic,
-        created_at,
-        votes,
-        comment_count,
-      }) => {
-        const timeFormatter = new Date(created_at).toDateString();
+    return (
+      <ul>
+        {articles.map(
+          ({
+            author,
+            title,
+            article_id,
+            topic,
+            created_at,
+            votes,
+            comment_count,
+          }) => {
+            const timeFormatter = new Date(created_at).toDateString();
 
-        return (
-          <div key={article_id} className="article-list-card">
-            <div className="article-list-card-text">
-              <div className="article-list-card-border">
-                <div>
-                  <p className="sublines-text">
-                    Posted by {author} on {timeFormatter}
-                  </p>
-                </div>
-                <span>
-                  {
-                    <Link
-                      to={`${article_id}`}
-                      className="underlined underlined--thick"
-                    >
-                      {title}
-                    </Link>
-                  }
-                </span>
+            return (
+              <li key={article_id} className="article-list-card">
+                <div className="article-list-card-text">
+                  <div className="article-list-card-border">
+                    <div>
+                      <p className="sublines-text">
+                        Posted by {author} on {timeFormatter}
+                      </p>
+                    </div>
+                    <span>
+                      {
+                        <Link
+                          to={`${article_id}`}
+                          className="underlined underlined--thick"
+                        >
+                          {title}
+                        </Link>
+                      }
+                    </span>
 
-                <div className="article-list-card-interactions">
-                  <span>
-                    <p>Topic: {utils.capitalizeFirstLetter(topic)}</p>
-                  </span>
-                  <span>
-                    <p>Comments: {comment_count}</p>
-                  </span>
-                  <div>
-                    <p>Votes: {votes}</p>
+                    <div className="article-list-card-interactions">
+                      <span>
+                        <p>Topic: {utils.capitalizeFirstLetter(topic)}</p>
+                      </span>
+                      <span>
+                        <p>Comments: {comment_count}</p>
+                      </span>
+                      <div>
+                        <p>Votes: {votes}</p>
+                      </div>
+                    </div>
                   </div>
                 </div>
-              </div>
-            </div>
-          </div>
-        );
-      }
+              </li>
+            );
+          }
+        )}
+      </ul>
     );
 };
 
--- src/components/cards/CommentsCard.jsx
+++ src/components/cards/CommentsCard.jsx
@@ -9,36 +9,40 @@ const CommentsCard = (props) => {
   if (isLoading) return <LoadingMessage />;
   if (err) return <ErrorMessage err={err} />;
   else
-    return comments.map(({ created_at, comment_id, author, votes, body }) => {
-      const timeFormatter = new Date(created_at).toDateString();
-      return (
-        <div className="comment-card-holder" key={`${comment_id}`}>
-          <div className="comment-card-text">
-            <div>
-              <div className="comment-card-bottom-row">
-                <div className="black voter">{author}</div>
-                <div className="date-article voter"> {timeFormatter}</div>
-                <div className="delete-comment voter">
-                  {author === user && (
-                    <button
-                      className="btn btn--border"
-                      value={comment_id}
-                      onClick={deleteComment}
-                    >
-                      Delete
-                    </button>
-                  )}
-                </div>
-                <div className="voter">
-                  <Voter votes={votes} comment_id={comment_id} />
+    return (
+      <ul>
+        {comments.map(({ created_at, comment_id, author, votes, body }) => {
+          const timeFormatter = new Date(created_at).toDateString();
+          return (
+            <li className="comment-card-holder" key={`${comment_id}`}>
+              <div className="comment-card-text">
+                <div>
+                  <div className="comment-card-bottom-row">
+                    <div className="black voter">{author}</div>
+                    <div className="date-article voter"> {timeFormatter}</div>
+                    <div className="delete-comment voter">
+                      {author === user && (
+                        <button
+                          className="btn btn--border"
+                          value={comment_id}
+                          onClick={deleteComment}
+                        >
+                          Delete
+                        </button>
+                      )}
+                    </div>
+                    <div className="voter">
+                      <Voter votes={votes} comment_id={comment_id} />
+                    </div>
+                  </div>
+                  <div className="comment-card-body-row">{body}</div>
                 </div>
               </div>
-              <div className="comment-card-body-row">{body}</div>
-            </div>
-          </div>
-        </div>
-      );
-    });
+            </li>
+          );
+        })}
+      </ul>
+    );
 };
 
 export default CommentsCard;
